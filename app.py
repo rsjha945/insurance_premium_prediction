b98a91e0883d659e55d621adea920dfc7ce32a1a
rsjha945 <ravishankarjha7108gmail.com>
2022-10-22 09:59:39 +0530

    model_trainer_config function configured

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    try:
-        raise Exception("We are testing custom exception")
-    except Exception as e:
-        insurance = InsuranceException(e,sys)
-        logging.info(insurance.error_message)
-        logging.info("We are testing logging module")
-    return "CI CD pipeline has been established."
+    #try:
+        #raise Exception("We are testing custom exception")
+    #except Exception as e:
+        #insurance = InsuranceException(e,sys)
+        #logging.info(insurance.error_message)
+        #logging.info("We are testing logging module")
+    return "CI CD pipeline has been established check it."
 
 
 if __name__=="__main__":
